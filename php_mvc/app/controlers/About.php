e72bcf04947258d858ac0ad059907e5270983e2d
giansoniaputra <giansonia555@gmail.com>
2022-11-03 14:05:13 +0700

    #View

--- php_mvc/app/controlers/About.php
+++ php_mvc/app/controlers/About.php
@@ -1,8 +1,8 @@
 <?php 
 
 class About {
-    public function index($nama ="Gian Sonia", $pekerjaan="Fornt End Developer"){
-        echo "Hallo nama saya $nama, saya adalah $pekerjaan";
+    public function index($nama ="Gian Sonia", $pekerjaan="Fornt End Developer", $umur = 32 ){
+        echo "Hallo nama saya $nama, saya adalah $pekerjaan saya berumur $umur Tahun";
     }
     public function page(){
         echo 'about/page';
